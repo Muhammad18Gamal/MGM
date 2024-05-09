b1c59ebc39bd6b85de5fb26bbd392c240603316a
Muhammad18Gamal <mg.mhmdgamal@gmail.com>
2024-05-09 21:42:46 +0300

    Basic BackEnd

---
+++ src/main/java/com/example/project1/WeatherApp.java
@@ -0,0 +1,41 @@
+/* 
+Version 1.0 - Initial Structure
+*/
+package com.example.project1;
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import java.net.HttpURLConnection;
+
+
+public class WeatherApp {
+    public static JSONObject getWeatherData(String locationName){
+        return null;
+    }
+
+    public static JSONArray getLocationData(String locationName){
+        return null;
+    }
+
+    private static HttpURLConnection fetchApiResponse(String urlString){
+        return null;
+    }
+
+    private static int findIndexOfCurrentTime(JSONArray timeList){
+        return 0;}
+
+    private static String getCurrentTime(){
+            return "0";
+        }
+    }
+
+
+
+
+
+
+
+
+
+
