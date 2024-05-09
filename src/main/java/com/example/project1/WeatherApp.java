bbb9e3bce84ec1f6f0951648f8c6dc3610870e00
Muhammad18Gamal <mg.mhmdgamal@gmail.com>
2024-05-09 21:51:31 +0300

    Added Methods Implementation

--- src/main/java/com/example/project1/WeatherApp.java
+++ src/main/java/com/example/project1/WeatherApp.java
@@ -1,41 +1,130 @@
 /* 
-Version 1.0 - Initial Structure
+Version 1.1 - Basic Weather Data Retrieval
+This version focuses solely on retrieving basic weather data based on a location name.
 */
 package com.example.project1;
-
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import java.net.HttpURLConnection;
+import org.json.simple.parser.JSONParser;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class WeatherApp {
-    public static JSONObject getWeatherData(String locationName){
-        return null;
-    }
+    public static JSONObject getWeatherData(String locationName) {
+        JSONArray locationData = getLocationData(locationName);
+        JSONObject location = (JSONObject) locationData.get(0);
+        double latitude = (double) location.get("latitude");
+        double longitude = (double) location.get("longitude");
 
-    public static JSONArray getLocationData(String locationName){
-        return null;
-    }
+        String urlString = "https://api.open-meteo.com/v1/forecast?" +
+                "latitude=" + latitude + "&longitude=" + longitude +
+                "&hourly=temperature_2m,relativehumidity_2m,weathercode,windspeed_10m&timezone=America%2FLos_Angeles";
 
-    private static HttpURLConnection fetchApiResponse(String urlString){
-        return null;
-    }
+        try {
+            HttpURLConnection conn = fetchApiResponse(urlString);
 
-    private static int findIndexOfCurrentTime(JSONArray timeList){
-        return 0;}
+            if (conn.getResponseCode() != 200) {
+                System.out.println("Error: Could not connect to API");
+                return null;
+            }
 
-    private static String getCurrentTime(){
-            return "0";
+            StringBuilder resultJson = new StringBuilder();
+            Scanner scanner = new Scanner(conn.getInputStream());
+            while (scanner.hasNext()) {
+                resultJson.append(scanner.nextLine());
+            }
+
+            scanner.close();
+            conn.disconnect();
+
+            JSONParser parser = new JSONParser();
+            JSONObject resultJsonObj = (JSONObject) parser.parse(String.valueOf(resultJson));
+            JSONObject hourly = (JSONObject) resultJsonObj.get("hourly");
+            JSONArray time = (JSONArray) hourly.get("time");
+            int index = findIndexOfCurrentTime(time);
+
+            JSONArray temperatureData = (JSONArray) hourly.get("temperature_2m");
+            double temperature = (double) temperatureData.get(index);
+
+            JSONArray relativeHumidity = (JSONArray) hourly.get("relativehumidity_2m");
+            long humidity = (long) relativeHumidity.get(index);
+
+            JSONArray windspeedData = (JSONArray) hourly.get("windspeed_10m");
+            double windspeed = (double) windspeedData.get(index);
+
+            JSONObject weatherData = new JSONObject();
+            weatherData.put("temperature", temperature);
+            weatherData.put("humidity", humidity);
+            weatherData.put("windspeed", windspeed);
+
+            return weatherData;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return null;
     }
 
+    private static JSONArray getLocationData(String locationName) {
+        locationName = locationName.replaceAll(" ", "+");
+        String urlString = "https://geocoding-api.open-meteo.com/v1/search?name=" +
+                locationName + "&count=10&language=en&format=json";
 
+        try {
+            HttpURLConnection conn = fetchApiResponse(urlString);
 
+            if (conn.getResponseCode() != 200) {
+                System.out.println("Error: Could not connect to API");
+                return null;
+            } else {
+                StringBuilder resultJson = new StringBuilder();
+                Scanner scanner = new Scanner(conn.getInputStream());
+                while (scanner.hasNext()) {
+                    resultJson.append(scanner.nextLine());
+                }
+                scanner.close();
+                conn.disconnect();
 
+                JSONParser parser = new JSONParser();
+                JSONObject resultsJsonObj = (JSONObject) parser.parse(String.valueOf(resultJson));
+                JSONArray locationData = (JSONArray) resultsJsonObj.get("results");
+                return locationData;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
+    private static HttpURLConnection fetchApiResponse(String urlString) {
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            return conn;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
+    private static int findIndexOfCurrentTime(JSONArray timeList) {
+        String currentTime = getCurrentTime();
+        for (int i = 0; i < timeList.size(); i++) {
+            String time = (String) timeList.get(i);
+            if (time.equalsIgnoreCase(currentTime)) {
+                return i;
+            }
+        }
+        return 0;
+    }
 
-
-
-
+    private static String getCurrentTime() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH':00'"));
+    }
+}
